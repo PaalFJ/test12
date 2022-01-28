e42534acd9316e3684357d9e9dca8a6aee9ff78f
paalfj <paalfolmer@gmail.com>
2022-01-28 18:37:33 +0100

    test

---
+++ test.go
@@ -0,0 +1,20 @@
+    import (
+    "fmt"
+    "rsc.io/quote"
+    )
+
+    func Hello() {
+    fmt.Println(quote.Hello())
+    }
+
+    func Opt(){
+    fmt.Println(quote.Opt())
+    }
+
+    func Glass(){
+    fmt.Println(quote.Glass())
+    }
+
+    func Go(){
+        fmt.Println(quote.Go())
+        }
